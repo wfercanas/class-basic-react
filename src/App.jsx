b6a503f38b20c477000f6ddee65ade7a0793a142
Fernando Cañas <fernando.canas@outlook.com>
2025-06-12 10:34:23 -0500

    feat: add layout to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { Keyboard } from "./components/Keyboard";
 
+import { StyledApp } from "./styles";
+
 function App() {
   const word = "ahorcado";
   function handleClick(event) {
     console.log(word.toUpperCase().includes(event.target.value));
   }
 
-  return <Keyboard onClick={handleClick} />;
+  return (
+    <StyledApp>
+      <Keyboard onClick={handleClick} />;
+    </StyledApp>
+  );
 }
 
 export default App;
---
+++ src/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const StyledApp = styled.main`
+  display: flex;
+  margin-top: 80px;
+  flex-direction: column;
+  gap: 64px;
+  align-items: center;
+`;
+
+export { StyledApp };
